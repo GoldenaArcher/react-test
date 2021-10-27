b9f7b7a5d5dae9252b39c8685e89c6168eb2b6fb
Lu Han <lu.han9467@gmail.com>
2021-10-27 12:47:19 -0600

    setup wallaby

--- eslint-intergration/palindromes.js
+++ eslint-intergration/palindromes.js
@@ -1,3 +1,3 @@
 module.exports = (str) => {
-  return str.split().reverst().join('');
+  return str.split().reverse().join('');
 };
--- eslint-intergration/tests/palindromes.test.js
+++ eslint-intergration/tests/palindromes.test.js
@@ -2,6 +2,6 @@ const palindrome = require('../palindromes');
 
 describe('palindrome()', () => {
   it('test for palindrome', () => {
-    expect(palindrome('1001')).tobe('1001');
+    expect(palindrome('1001')).toBe('1001');
   });
 });
